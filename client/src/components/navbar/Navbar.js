8deeef843ec81ef304955af8f9849eb3fb78b953
aniketjha1729 <kumaraniket1729@gmail.com>
2021-05-02 03:27:06 +0530

    frontend demo

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
+      <p>
+      <i class="fas fa-users"></i> Welcome
+      </p>
       <Link to="/admin/dashboard">CLick me</Link>
     </div>
   );

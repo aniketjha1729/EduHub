187c532988f645f4dc59b40166149eb4cf6e6bed
aniketjha1729 <kumaraniket1729@gmail.com>
2021-05-04 02:35:54 +0530

    admin signin ui

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import AdminDashboard from "../admin/AdminDashboard";
+import AdminSignIn from "../admin/AdminSignIn";
+
 const Routes = () => {
   return (
     <section className="container">
       <Switch>
         <Route exact path="/admin/dashboard" component={AdminDashboard} />
+        <Route exact path="/admin/signin" component={AdminSignIn} />
       </Switch>
     </section>
   );

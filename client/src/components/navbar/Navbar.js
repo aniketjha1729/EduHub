784829c5fd48db2e0fdf12ae385626aa7a2c259a
aniketjha1729 <kumaraniket1729@gmail.com>
2021-05-05 00:58:30 +0530

    backend intergrated

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./css/navbar.css";
+import axios from "../../api/axios";
 const Navbar = () => {
+  useEffect(() => {
+    const apiCall = async () => {
+      try {
+        axios.get("/user/test").then(({data}) => {
+          console.log(data);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    apiCall();
+  }, []);
   return <div className="navbar_main">Navbar</div>;
 };
 

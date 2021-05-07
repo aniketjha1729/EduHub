35204686641e6668671434807bcec5206f499f44
aniketjha1729 <kumaraniket1729@gmail.com>
2021-05-07 22:58:06 +0530

    component

--- client/src/components/admin/AdminDashboard.js
+++ client/src/components/admin/AdminDashboard.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./css/adminDashboard.css";
+import AdminDashboardTable from "./AdminDashboardTable";
 const AdminDashboard = () => {
   return (
     <div className="container">
       <div className="dashboard">
         <div className="row">
           <div className="col-2 dashborad_left">
-          <br/>
+            <br />
             <div className="dashborad_left_header">
-            <i class="fas fa-chalkboard-teacher" size="3x"></i> &nbsp;&nbsp;
+              <i class="fas fa-chalkboard-teacher" size="3x"></i> &nbsp;&nbsp;
               <b>EduHub</b>
             </div>
             <div className="dashborad_left_menu">
@@ -19,101 +20,16 @@ const AdminDashboard = () => {
           </div>
           <div className="col-10 dashborad_right">
             <br />
-            <div className="row justify-content-around dashborad_right_menu">
-              <div className="col-2 dashborad_right_menu_item">
-                <b>Total Users</b>
-                <div>
-                  <i class="fas fa-users"></i> &nbsp;50
-                </div>
-              </div>
-              <div className="col-2 dashborad_right_menu_item">
-                <b>Verified User</b>
-                <div>
-                  <i class="fas fa-users-cog"></i> &nbsp;25
-                </div>
-              </div>
-              <div className="col-2 dashborad_right_menu_item">
-                <b>Posts</b>
-                <div>
-                  <i class="fas fa-comments"></i> &nbsp; 120
-                </div>
-              </div>
-            </div>
-            <div className="dashborad_right_table">
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th scope="col">S.No</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>
-                      <button type="button" className="btn btn-primary">
-                        Primary
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>
-                      <button type="button" className="btn btn-primary">
-                        Primary
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>
-                      <button type="button" className="btn btn-primary">
-                        Primary
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">4</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>
-                      <button type="button" className="btn btn-primary">
-                        Primary
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">5</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>
-                      <button type="button" className="btn btn-primary">
-                        Primary
-                      </button>
-                    </td>
-                  </tr>
-                  <tr style={{width:"30%"}}>
-                    <th scope="row">6</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>
-                      <button type="button" className="btn btn-primary">
-                        Primary
-                      </button>
-                      
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <AdminDashboardTable
+              totalUser="Total User"
+              userCount="130"
+              verifiedUser="Verfied User"
+              noOfVerifiedUser="50"
+              teacher="Teacher"
+              noOfTeacher="30"
+              student="Student"
+              noOfStudent="52"
+            />
           </div>
         </div>
       </div>

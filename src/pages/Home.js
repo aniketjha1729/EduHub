7d27edb3103603e3eeac144e7443a763c15a192f
aniketjha1729 <kumaraniket1729@gmail.com>
2021-05-29 05:21:22 +0530

    basic layout of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,69 @@
 import React from "react";
-
+import "./home.css"
 const Home = () => {
   return <>
-    
+    <div className="home-container">
+      <div className="home-left-view">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo blanditiis quod vitae ut. Eveniet nam perspiciatis, in reprehenderit molestias, assumenda quia id molestiae deleniti vero libero sapiente dicta sunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo blanditiis quod vitae ut. Eveniet nam perspiciatis, in reprehenderit molestias, assumenda quia id molestiae deleniti vero libero sapiente dicta sunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo blanditiis quod vitae ut. Eveniet nam perspiciatis, in reprehenderit molestias, assumenda quia id molestiae deleniti vero libero sapiente dicta sunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo blanditiis quod vitae ut. Eveniet nam perspiciatis, in reprehenderit molestias, assumenda quia id molestiae deleniti vero libero sapiente dicta sunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo blanditiis quod vitae ut. Eveniet nam perspiciatis, in reprehenderit molestias, assumenda quia id molestiae deleniti vero libero sapiente dicta sunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo blanditiis quod vitae ut. Eveniet nam perspiciatis, in reprehenderit molestias, assumenda quia id molestiae deleniti vero libero sapiente dicta sunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo blanditiis quod vitae ut. Eveniet nam perspiciatis, in reprehenderit molestias, assumenda quia id molestiae deleniti vero libero sapiente dicta sunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo blanditiis quod vitae ut. Eveniet nam perspiciatis, in reprehenderit molestias, assumenda quia id molestiae deleniti vero libero sapiente dicta sunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo blanditiis quod vitae ut. Eveniet nam perspiciatis, in reprehenderit molestias, assumenda quia id molestiae deleniti vero libero sapiente dicta sunt.
+      </div>
+      <div className="home-middle-view">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus esse, nostrum culpa, corporis ducimus laborum, quasi reprehenderit eligendi blanditiis fuga ab omnis neque laudantium repudiandae rem tenetur. Nemo, voluptatem?d
+      </div>
+      <div className="home-right-view">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque quas id fuga officiis obcaecati, consequatur sunt iusto expedita saepe! Voluptatum modi minima ducimus expedita. Cum nemo culpa id itaque iusto.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque quas id fuga officiis obcaecati, consequatur sunt iusto expedita saepe! Voluptatum modi minima ducimus expedita. Cum nemo culpa id itaque iusto.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque quas id fuga officiis obcaecati, consequatur sunt iusto expedita saepe! Voluptatum modi minima ducimus expedita. Cum nemo culpa id itaque iusto.
+      </div>
+    </div>
   </>;
 };
 

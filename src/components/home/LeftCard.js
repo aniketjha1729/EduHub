74217ffdb9aa42af724b3c6a8a2b8868f382273a
aniketjha1729 <kumaraniket1729@gmail.com>
2021-05-30 19:12:18 +0530

    home page data displayed 0.1

---
+++ src/components/home/LeftCard.js
@@ -0,0 +1,241 @@
+import React from "react";
+
+const LeftCard = () => {
+  return (
+    <div>
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div class="card" style={{ width: "100%" }}>
+        <div class="card-header">Featured</div>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <br />
+    </div>
+  );
+};
+
+export default LeftCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
+import LeftCard from "../components/home/LeftCard";
+import NoticeCard from "../components/home/NoticeCard";
+import PostCard from "../components/home/PostCard";
+
 import "./home.css";
 const Home = () => {
   return (
     <>
       <div className="home-container">
-        <div className="home-left-view"></div>
-        <div className="home-middle-view"></div>
-        <div className="home-right-view"></div>
+        <div className="home-left-view">
+          <LeftCard />
+        </div>
+        <div className="home-middle-view">
+          <PostCard />
+        </div>
+        <div className="home-right-view">
+          <NoticeCard />
+        </div>
       </div>
     </>
   );

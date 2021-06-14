dca26ff4c4bed4c5364c1d666657826f6a95d917
Amit-CBS <cbs.amitrajsingh@gmail.com>
2021-06-14 22:29:15 +0530

    Team

--- src/pages/Teams.js
+++ src/pages/Teams.js
@@ -3,38 +3,42 @@ import React from "react";
 const Teams = () => {
   return <div className="container" style={{"padding-top":"60px"}}>
     <div className="row">
-      <div className="col-6 teamCol teamName">
+      <div className="col-5 teamCol teamName teamCardUp teamCardLeft">
         <img className="teamPic" src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar"></img>
           Aniket Kumar
       </div>
-      <div className="col-6 teamCol teamName">
+      <div className="col-1"></div>
+      <div className="col-5 teamCol teamName teamCardUp teamCardRight">
         <img className="teamPic" src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar"></img>
           Amit Rai
       </div>
     </div>
     <div className="row">
-      <div className="col teamCol">
+      <div className="col-5 teamCol teamCardDown teamCardLeft teamNameID">
         <i className="fab fa-github fa-2x"> https://github.com/aniketjha1729</i>
       </div>
-      <div className="col teamCol">
-        <i className="fab fa-github fa-2x"> https://github.com/amitRaiIDbatao</i>
+      <div className="col-1"></div>
+      <div className="col-5 teamCol teamCardDown teamCardRight teamNameID">
+        <i className="fab fa-github fa-2x"> https://github.com/ardev472</i>
       </div>
     </div>
     <div className="row">
-      <div className="col-6 teamCol teamName">
+      <div className="col-5 teamCol teamName teamCardUp teamCardLeft">
         <img className="teamPic" src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar"></img>
           Amit Raj Singh
       </div>
-      <div className="col-6 teamCol teamName">
+      <div className="col-1"></div>
+      <div className="col-5 teamCol teamName teamCardUp teamCardRight">
         <img className="teamPic" src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar"></img>
           Sharad Baid
       </div>
     </div>
     <div className="row">
-      <div className="col teamCol">
+      <div className="col-5 teamCol teamCardDown teamCardLeft teamNameID">
         <i className="fab fa-github fa-2x"> https://github.com/Amit-CBS</i>
       </div>
-      <div className="col teamCol">
+      <div className="col-1"></div>
+      <div className="col-5 teamCol teamCardDown teamCardRight teamNameID">
         <i className="fab fa-github fa-2x"> https://github.com/sharad16j</i>
       </div>
     </div>

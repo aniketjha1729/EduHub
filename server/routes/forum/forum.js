e17a5e519e27eff8312f46b850552d0a262ba27c
aniketjha1729 <kumaraniket1729@gmail.com>
2021-06-18 09:11:26 +0530

    forum ques ans 1.0

--- server/routes/forum/forum.js
+++ server/routes/forum/forum.js
@@ -9,6 +9,11 @@ const {
   getAnswerByQuestion,
   getQuestionByDept,
   getQuestionByTags,
+  addReply,
+  upVoteQuestion,
+  downVoteQuestion,
+  upVoteAns,
+  downVoteAns,
 } = require("../../controllers/forum/forum");
 
 router.post("/question", isUserAuth, isVerified, question);
@@ -18,6 +23,21 @@ router.post("/answer/:quesId", isUserAuth, isVerified, answer);
 router.get("/allQuestions", isUserAuth, isVerified, allQuestion);
 
 router.post("/questions/tags", isUserAuth, isVerified, getQuestionByTags);
+
+router.post("/reply/comment/:commentId", isUserAuth, isVerified, addReply);
+
+router.put("/question/upVote/:quesId", isUserAuth, isVerified, upVoteQuestion);
+
+router.put(
+  "/question/downVote/:quesId",
+  isUserAuth,
+  isVerified,
+  downVoteQuestion
+);
+
+router.put("/answer/upVote/:ansId", isUserAuth, isVerified, upVoteAns);
+
+router.put("/answer/downVote/:ansId", isUserAuth, isVerified, downVoteAns);
 
 router.get(
   "/question/department/:deptName",

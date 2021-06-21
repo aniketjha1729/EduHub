fc8e766abd95489ab90c97e373545353681539c2
Amit-CBS <cbs.amitrajsingh@gmail.com>
2021-06-22 04:42:35 +0530

    Forum and TeamCardTransition

--- src/components/forum/ForumRight.js
+++ src/components/forum/ForumRight.js
@@ -4,7 +4,20 @@ const ForumRight = () =>
 {
   return(
     <div>
-      Here goes 🎓 
+      <form>
+        <textarea
+          rows="15"
+          cols="23"
+          type="textarea"
+          name="question"
+          className="form-control UserHome-example-input example-input"
+          id="ForumRightIDtextarea"
+          // value={state.content || ""}
+          placeholder="Type Question here"
+          // onChange={handleInputChange}
+        />
+        <input id="ForumRightIDbutton" type="submit" value="Submit" />
+      </form>
     </div>
   )
 }

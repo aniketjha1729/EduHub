6f225051a805777bd86a1055067d1cfa0c36446b
aniketjha1729 <kumaraniket1729@gmail.com>
2021-06-30 12:10:51 +0530

    post and reoly added

--- src/components/forum/ForumFeed.js
+++ src/components/forum/ForumFeed.js
@@ -4,17 +4,12 @@ import axios from "../../api/axios";
 import "./forum.css";
 
 const ForumFeed = () => {
-  const [toggleMore, setToggleMore] = useState(false);
-  const [toggleAnswer, setToggleAnswer] = useState(false);
+  const [answerId, setAnswerId] = useState();
+  const [replyId, setReplyId] = useState();
   const [getAllQues, setAllQues] = useState([]);
-
-  const toggleMoreAns = () => {
-    setToggleMore(!toggleMore);
-  };
-
-  const toggleAnswerSection = () => {
-    setToggleAnswer(!toggleAnswer);
-  };
+  const [getAnsByQues, setAnsByQues] = useState([]);
+  const [ans, setAns] = useState("");
+  const [reply, setReply] = useState("");
 
   const sortByDate = (a, b) => {
     if (a.date < b.date) {
@@ -26,13 +21,90 @@ const ForumFeed = () => {
     return 0;
   };
 
+  const toggleAnswerSection = (quesId) => {
+    setAnswerId(quesId);
+    getAnswerByQues(quesId);
+  };
+
+  const toggleReplySection = (ansId) => {
+    setReplyId(ansId);
+  };
+
+  const getAnswerByQues = async (quesId) => {
+    try {
+      const { data } = await axios.get(`/forum/answer/question/${quesId}`);
+      console.log(data);
+      const sortedData = data.sort(sortByDate);
+      setAnsByQues(sortedData);
+      console.log(sortedData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const upVoteQues = async (quesId) => {
+    try {
+      const { data } = await axios.put(`/forum/question/upVote/${quesId}`);
+      getAllQuestion();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const downVoteQues = async (quesId) => {
+    try {
+      const { data } = await axios.put(`/forum/question/downVote/${quesId}`);
+      getAllQuestion();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const upVoteAns = async (ansId, quesId) => {
+    try {
+      const { data } = await axios.put(`/forum/answer/upVote/${ansId}`);
+      getAnswerByQues(quesId);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const downVoteAns = async (ansId, quesId) => {
+    try {
+      const { data } = await axios.put(`/forum/answer/downVote/${ansId}`);
+      getAnswerByQues(quesId);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const postAnswer = async (ans, quesId) => {
+    const body = { ans };
+    console.log(ans);
+    try {
+      const { data } = await axios.post(`/forum/answer/${quesId}`, body);
+      getAnswerByQues(quesId);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const postReply = async (reply, ansId, quesId) => {
+    const body = { reply };
+    console.log(reply);
+    try {
+      const { data } = await axios.post(`/forum/reply/comment/${ansId}`, body);
+      getAnswerByQues(quesId);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const getAllQuestion = async () => {
     try {
       const { data } = await axios.get("/forum/allQuestions");
-      console.log(data);
       const sortedData = data.sort(sortByDate);
       setAllQues(sortedData);
-      console.log(sortedData);
     } catch (err) {
       console.log(err);
     }
@@ -48,7 +120,12 @@ const ForumFeed = () => {
         <div className="row forumBody">
           <div className="col-1 text-center">
             <div className="forum-question-voteUp">
-              <i className="fas fa-arrow-circle-up fa-3x"></i>
+              <i
+                className="fas fa-arrow-circle-up fa-3x"
+                onClick={() => {
+                  upVoteQues(question._id);
+                }}
+              ></i>
             </div>
             <div className="forum-question-voteCountUp">
               + {question.upVotes.length}
@@ -57,7 +134,12 @@ const ForumFeed = () => {
               - {question.downVotes.length}
             </div>
             <div className="forum-question-voteDowb">
-              <i className="fas fa-arrow-circle-down fa-3x"></i>
+              <i
+                className="fas fa-arrow-circle-down fa-3x"
+                onClick={() => {
+                  downVoteQues(question._id);
+                }}
+              ></i>
             </div>
           </div>
           <div className="col-11">
@@ -68,12 +150,7 @@ const ForumFeed = () => {
               >
                 <div className="forum-QuestionContainer">
                   <span>Q.</span>
-                  <span>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Veniam sed dolores cumque voluptatibus! Voluptas, recusandae
-                    nisi autem repellendus non, facilis saepe nemo ullam omnis
-                    quaerat debitis eveniet rem est in?
-                  </span>
+                  <span>{question.description}</span>
                 </div>
                 {question.document ? <ImageHelper quesId={question._id} /> : ""}
                 <div className="forum-tagsContainer">
@@ -86,7 +163,9 @@ const ForumFeed = () => {
                 <div className="row text-center forum-questionAction">
                   <div
                     className="col-6 forum-questionAction-answer"
-                    onClick={toggleAnswerSection}
+                    onClick={() => {
+                      toggleAnswerSection(question._id);
+                    }}
                   >
                     Answer
                   </div>
@@ -94,87 +173,126 @@ const ForumFeed = () => {
                     Save
                   </div>
                 </div>
-                {toggleAnswer ? (
+                {answerId == question._id ? (
                   <>
                     <h5 className="card-title">Answers:</h5>
-                    <div className="forumAnswerContainer">
-                      <div className="forumFirstAns">
-                        <div className="row">
-                          <div className="col-1 text-center">
-                            <div className="forum-answer-voteUp">
-                              <i className="fas fa-arrow-circle-up fa-2x"></i>
-                            </div>
-                            <div className="forum-answer-voteCount">45</div>
-                            <div className="forum-answer-voteDowb">
-                              <i className="fas fa-arrow-circle-down fa-2x"></i>
-                            </div>
-                          </div>
-                          <div className="col-11 forumAnswer">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Sed nulla repudiandae culpa labore laudantium
-                            corrupti, iste porro dolorem optio, velit provident,
-                            id omnis repellendus? Ut reprehenderit molestias ab
-                            itaque voluptatibus!
-                          </div>
-                          <div className="forum-replyButton">Reply</div>
-                        </div>
-                        <p className="forum-AnswerReply">
-                          Lorem ipsum dolor sit, amet consectetur adipisicing
-                          elit. Sed nulla repudiandae culpa
-                        </p>
-                        <p className="forum-AnswerReply">
-                          Lorem ipsum dolor sit, amet consectetur adipisicing
-                          elit. Sed nulla repudiandae culpa
-                        </p>
-                        <p className="forum-AnswerReply">
-                          Lorem ipsum dolor sit, amet consectetur adipisicing
-                          elit. Sed nulla repudiandae culpa
-                        </p>
-                      </div>
-                      <div
-                        className="forumShowMoreAns text-center"
-                        onClick={toggleMoreAns}
+                    <div className="ansInputWrapper">
+                      <form
+                        onSubmit={(e) => {
+                          e.preventDefault();
+                          postAnswer(ans, question._id);
+                          setAns("");
+                        }}
                       >
-                        Show More Answers
-                      </div>
-                      {toggleMore ? (
-                        <div className="forumNAns">
+                        <div className="form-group">
+                          <div className="row">
+                            <div className="col-10">
+                              <input
+                                type="text"
+                                className="form-control"
+                                name="ans"
+                                value={ans}
+                                onChange={(e) => setAns(e.target.value)}
+                                placeholder="Type Your answer"
+                              />
+                            </div>
+                            <div className="col-2 text-center">
+                              <button
+                                type="button"
+                                type="submit"
+                                className="btn btn-outline-primary"
+                              >
+                                Answer
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                    {getAnsByQues.map((ans) => (
+                      <div className="forumAnswerContainer">
+                        <div className="forumFirstAns">
                           <div className="row">
                             <div className="col-1 text-center">
                               <div className="forum-answer-voteUp">
-                                <i className="fas fa-arrow-circle-up fa-2x"></i>
+                                <i
+                                  className="fas fa-arrow-circle-up fa-2x"
+                                  onClick={() => {
+                                    upVoteAns(ans._id, question._id);
+                                  }}
+                                ></i>
                               </div>
-                              <div className="forum-answer-voteCount">45</div>
+                              <div className="forum-answer-voteCount">
+                                + {ans.upVotes.length}
+                              </div>
+                              <div className="forum-answer-voteCount">
+                                - {ans.downVotes.length}
+                              </div>
                               <div className="forum-answer-voteDowb">
-                                <i className="fas fa-arrow-circle-down fa-2x"></i>
+                                <i
+                                  className="fas fa-arrow-circle-down fa-2x"
+                                  onClick={() => {
+                                    downVoteAns(ans._id, question._id);
+                                  }}
+                                ></i>
                               </div>
                             </div>
                             <div className="col-11 forumAnswer">
-                              Lorem ipsum dolor sit, amet consectetur
-                              adipisicing elit. Sed nulla repudiandae culpa
-                              labore laudantium corrupti, iste porro dolorem
-                              optio, velit provident, id omnis repellendus? Ut
-                              reprehenderit molestias ab itaque voluptatibus!
+                              {ans.answer}
                             </div>
-                            <div className="forum-replyButton">Reply</div>
                           </div>
-                          <p className="forum-AnswerReply">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Sed nulla repudiandae culpa
-                          </p>
-                          <p className="forum-AnswerReply">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Sed nulla repudiandae culpa
-                          </p>
-                          <p className="forum-AnswerReply">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Sed nulla repudiandae culpa
-                          </p>
+                          <div
+                            className="forum-replyButton"
+                            onClick={() => {
+                              toggleReplySection(ans._id);
+                            }}
+                          >
+                            Reply
+                          </div>
+                          <div className="replyInputWrapper">
+                            {replyId == ans._id ? (
+                              <form
+                                onSubmit={(e) => {
+                                  e.preventDefault();
+                                  postReply(reply, ans._id, question._id);
+                                  setReply("");
+                                }}
+                              >
+                                <div className="row">
+                                  <div className="col-10">
+                                    <div className="form-group">
+                                      <input
+                                        type="text"
+                                        value={reply}
+                                        onChange={(e) =>
+                                          setReply(e.target.value)
+                                        }
+                                        className="form-control"
+                                        placeholder="Reply To Comments"
+                                      />
+                                    </div>
+                                  </div>
+                                  <div className="col-2">
+                                    <button
+                                      type="button"
+                                      type="submit"
+                                      className="btn btn-outline-primary"
+                                    >
+                                      Reply
+                                    </button>
+                                  </div>
+                                </div>
+                              </form>
+                            ) : (
+                              ""
+                            )}
+                          </div>
                         </div>
-                      ) : (
-                        ""
-                      )}
-                    </div>
+                        {ans.replies.map((reply) => (
+                          <p className="forum-AnswerReply">{reply.reply}</p>
+                        ))}
+                      </div>
+                    ))}
                   </>
                 ) : (
                   ""
